42193fb3af149991484309ec92820691492ff7ae
salimovrashad <selimovresad33@gmail.com>
2024-03-08 21:32:17 +0400

    add firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD_YVKsMZnA6BuUVjqSognVDktOn0if6uU",
-  authDomain: "idrobe-az.firebaseapp.com",
-  projectId: "idrobe-az",
-  storageBucket: "idrobe-az.appspot.com",
-  messagingSenderId: "172212866550",
-  appId: "1:172212866550:web:81f1993d02aea749718bd7"
+  apiKey: "AIzaSyC8nc8VV748MciWKAL0MqUrr1MOIusxEjM",
+  authDomain: "idrobeaz.firebaseapp.com",
+  projectId: "idrobeaz",
+  storageBucket: "idrobeaz.appspot.com",
+  messagingSenderId: "743463639879",
+  appId: "1:743463639879:web:b04134697ab96b1e9fca62"
 };
 
 // Initialize Firebase
